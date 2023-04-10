06b9d80d2156b2199044a9995d3b59a3fa6ed6cb
hemakiran0 <112335606+hemakiran0@users.noreply.github.com>
2023-04-10 15:34:13 +0530

    Add files via upload

---
+++ chatapp.py
@@ -0,0 +1,32 @@
+import gradio as gr
+import openai
+
+openai.api_key = open("openaiAPIKEY.txt", "r").read().strip('\n')
+
+message_history = []
+
+
+def predict(input):
+    global message_history
+    message_history.append({"role": "user", "content": input})
+    # prompt = f"""{message_history[0]["content"]}
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history)
+
+    reply_content = completion.choices[0].message.content
+    print(reply_content)
+    message_history.append({"role": "assistant", "content": reply_content})
+    response = [(message_history[i]["content"], message_history[i+1]["content"]) for i in range(0, len(message_history)-1,2)]
+    return response
+
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    with gr.Row():
+        txt = gr.Textbox(show_label=False, placeholder="Type your message here").style(container=False)
+        txt.submit(predict, txt, chatbot)
+        #txt.submit(lambda: "",None, txt)
+        #txt.submit(None, None, txt, js="() => {''}")
+
+demo.launch()
